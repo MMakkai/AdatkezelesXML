949887c82266b97c53b5450308bf26f727f6ba80
MMakkai <markmakkai97@gmail.com>
2020-12-10 09:32:43 +0100

    Utolsó frissités
    Utolsó simitások a DOMModifyon és a Jegyzőkönyv frissítése.

--- XMLFeladatCDTD5F/DOMParseCDTD5F/src/hu/domparse/cdtd5f/DOMModifyCDTD5F.java
+++ XMLFeladatCDTD5F/DOMParseCDTD5F/src/hu/domparse/cdtd5f/DOMModifyCDTD5F.java
@@ -12,7 +12,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -23,38 +22,36 @@ public class DOMModifyCDTD5F {
 	public static void main(String argv[]) {
 
 	       try {
+	    	   //Elérési útvonala az XML filenak
 	        String filepath = "XMLCDTD5F.xml";
 	        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 	        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 	        Document doc = docBuilder.parse(filepath);
 
-	        // Get the staff element , it may not working if tag has spaces, or
-	        // whatever weird characters in front...it's better to use
-	        // getElementsByTagName() to get it directly.
-	        // Node staff = company.getFirstChild();
+	        
 
-	        // Get the staff element by tag name directly
+	        // Megszerzi a videojatek elemet a neve alapján
 	        Node videojatek = doc.getElementsByTagName("videojatek").item(0);
 
-	        // append a new node to staff
+	        // Hozzáad egy új adatot a videojatek gyerekhez
 	        Element ertekeles = doc.createElement("ertekeles");
 	        ertekeles.appendChild(doc.createTextNode("5/5"));
 	        videojatek.appendChild(ertekeles);
 
-	        // loop the staff child node
+	        // Ciklusban végig megy a videojatek gyereken
 	        NodeList list = videojatek.getChildNodes();
 
 	        for (int i = 0; i < list.getLength(); i++) {
 	            
 	                   Node node = list.item(i);
 
-	           // get the salary element, and update the value
+	           // Megszerzi az ar elemet és frissiti új értékkel.
 	           if ("ar".equals(node.getNodeName())) {
 	            node.setTextContent("21000");
 	           }
 	        }
 
-	        // write the content into xml file
+	        // Módosítás egy új xml fileban történik, amit XMLCDTD5F_edit.xml-nek hivnak.
 	        TransformerFactory transformerFactory = TransformerFactory.newInstance();
 	        Transformer transformer = transformerFactory.newTransformer();
 	        DOMSource source = new DOMSource(doc);
